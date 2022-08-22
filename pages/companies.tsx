e796523247900037bbc39f3870a9ecee0542b290
PierreBou <pierre.bouriat@outlook.fr>
2022-08-22 23:30:57 +0200

    WIP: fix the next routing issue with company/companies

--- pages/companies.tsx
+++ pages/companies.tsx
@@ -8,24 +8,28 @@ type Props = {};
 
 const Companies = (props: Props) => {
   const companies = useQuery(["companies"], async () => {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/companies");
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_API_BASE_URL + "/companies/"
+    );
     return await response.json();
-  })
-  const router = useRouter()
+  });
+  const router = useRouter();
 
   return (
     <div className="container">
-      {companies.status === "success" && companies.data.map((company: any) => (
-        <div key={company.id} className={styles.company}>
-          <Link href={router.pathname + "companies/" + company.id}>
-            <a>
-              <h3>{company.name}</h3>
-              <p>{company.createdAt}</p>
-            </a>
-          </Link>
-        </div>
-      ))}
-    </div>);
+      {companies.status === "success" &&
+        companies.data.map((company: any) => (
+          <div key={company.id} className={styles.company}>
+            <Link href={router.pathname + company.id}>
+              <a>
+                <h3>{company.name}</h3>
+                <p>{company.createdAt}</p>
+              </a>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Companies;
--- pages/companies/[id].tsx
+++ pages/companies/[id].tsx
@@ -1,28 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Company = (props: Props) => {
-    const router = useRouter()
-    const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
-    const company = useQuery(["company"], async () => {
-        const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/companys/" + id);
-        return await response.json();
-    })
+  const company = useQuery(["company"], async () => {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_API_BASE_URL + "/companies/" + id
+    );
+    return await response.json();
+  });
 
-    if (company.status === "success") {
-        return (
-            <div className='container'>
-                <h1>{company.data.title}</h1>
-                <h2>{company.data.createdAt}</h2>
-                <h2>{company.data.status}</h2>
-                <p>{company.data.description}</p>
-            </div>
-        )
-    }
-}
+  if (company.status === "success") {
+    return (
+      <div className="container">
+        <h1>{company.data.title}</h1>
+        <h2>{company.data.createdAt}</h2>
+        <h2>{company.data.status}</h2>
+        <p>{company.data.description}</p>
+      </div>
+    );
+  }
+};
 
-export default Company
+export default Company;
